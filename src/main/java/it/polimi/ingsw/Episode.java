a180ffd8ea34051a2a2c397db78507ebe0e93d43
AlessioPiovesan <alessio.piovesan@mail.polimi.it>
2023-03-13 15:14:31 +0100

    first steps

--- src/main/java/it/polimi/ingsw/Episode.java
+++ src/main/java/it/polimi/ingsw/Episode.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Episode{
+    String title;
     DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
     DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
     String type;
     String res;
-    Serie nameSerie;
+    String nameSerie;
     int s;
     int e;
     int dim;
@@ -23,6 +24,42 @@ public class Episode{
     Languages sub;
     ExtensionType exten;
 
-    Episode ( ){
+    public Episode (DateTimeFormatter dayFormatter, DateTimeFormatter timeFormatter, String type, String res, String nameSerie, int s, int e, int dim, int w, int h, int diff, float mbs, LocalDateTime view, LocalDateTime len, LocalDateTime prime, Languages lang, Languages sub, ExtensionType exten){
+        title = makeTitle(nameSerie, s, e);
+        this.dayFormatter = dayFormatter;
+        this.timeFormatter = timeFormatter;
+        this.type = type;
+        this.res = res;
+        this.nameSerie = nameSerie;
+        this.s = s;
+        this.e = e;
+        this.dim = dim;
+        this.w = w;
+        this.h = h;
+        this.diff = diff;
+        this.mbs = mbs;
+        this.view = view;
+        this.len = len;
+        this.prime = prime;
+        this.lang = lang;
+        this.sub = sub;
+        this.exten = exten;
+    }
+
+    private String makeTitle (String nameSerie, int s, int e){
+        String name;
+        if (e < 10)
+            if (s < 10) name = nameSerie + " - s0" + s + "e00" + e;
+            else name = nameSerie + " - s" + s + "e00" + e;
+        else if (e < 100)
+            if (s < 10) name = nameSerie + " - s0" + s + "e0" + e;
+            else name = nameSerie + " - s" + s + "e0" + e;
+        else if (s < 10) name = nameSerie + " - s0" + s + "e" + e;
+        else name = nameSerie + " - s" + s + "e" + e;
+        return name;
+    }
+
+    public String getTitle ( ){
+        return title;
     }
 }
--- src/main/java/it/polimi/ingsw/Movie.java
+++ src/main/java/it/polimi/ingsw/Movie.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw;
 
 public class Movie{
+    private final String title;
+
+    public Movie (String title){
+        this.title = title;
+    }
+
+    public String getTitle ( ){
+        return title;
+    }
 }
